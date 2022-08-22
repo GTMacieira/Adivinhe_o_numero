4f9b9f8ebb3b3af1757fb776fad1fb7592d5c6ec
GTMacieira <macieira.gtalpo@gmail.com>
2022-08-21 21:57:14 -0300

    Primeiros passos

---
+++ Advinhe_o_numero.py
@@ -0,0 +1,10 @@
+#importar a biblioteca random
+import random
+
+#gerar um número aleatório ente 1 e 100 e salvar na variável n_sorteado
+n_soerteado = random.randrange(1,100)
+print(n_soerteado)
+
+#solicitar o nome do usuário
+print("Olá, qual seu nome?")
+input()
