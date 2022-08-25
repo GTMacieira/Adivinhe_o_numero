8362171f57befaf82d740dfb855019a7a56f94bd
GTMacieira <macieira.gtalpo@gmail.com>
2022-08-24 21:46:53 -0300

    implementado o sistema de pontos

--- Advinhe_o_numero.py
+++ Advinhe_o_numero.py
@@ -4,8 +4,10 @@ import random
 def ch_difficulty(pl_name):
     # define a dificuldade do jogo
     print(f'{pl_name}, escolha de acordo com os números a dificuldade do jogo:')
-    difficulty = input('1 - facíl \n2 - médio \n3 - difícil \n4 - insano\n')
+    difficulty = input('1 - Fácíl \n2 - Médio \n3 - Difícil \n4 - Insano\n')
+    #identifica se a dificuldade dgitada é um número
     if difficulty.isnumeric():
+        #identifica se a dificuldade selecionada está entre as disponiveis
         if 1 >= int(difficulty) or int(difficulty) <= 4:
             return difficulty
         else:
@@ -17,15 +19,17 @@ def ch_difficulty(pl_name):
 
 
 
-def play (drawn_n, pl_name,opportunity,attempts):
+def play (drawn_n, pl_name, opportunity, attempts, points):
     #print(drawn_n)
     ch_number = input(f'{pl_name}, ESCOLHA SEU NÚMERO ENTRE 1 e 50\n')
     attempts.append(ch_number)
+    #identifica se o número escolhido pelo usuário é o número sorteado
     if int(ch_number) == drawn_n:
         print(f'PARABÉNS {pl_name}!!!!\n'
-              f'Você acertou o número sorteado')
+              f'Você acertou o número sorteado e fez {points} pontos')
         exit()
     else:
+        #caso o usuário erre o número identifica se é maior ou menor que o escolhido
         if int(ch_number) > drawn_n:
             print(f'{pl_name}, seu número é maior que o número sorteado')
             print(f'Suas tentativas foram {attempts}')
@@ -38,7 +42,7 @@ def play (drawn_n, pl_name,opportunity,attempts):
             return opportunity
 
 #solicitar o nome do usuário
-pl_name = input ("Olá, qual seu nome?\n")
+pl_name = input ("Olá, qual seu nome?\n").capitalize()
 
 difficulty = int(ch_difficulty(pl_name))
 
@@ -55,14 +59,17 @@ elif difficulty == 3:
 elif difficulty == 4:
     opportunity = 2
 
+points = 100
+lost_points = points / opportunity
+
 print(f'{pl_name}, Vamos  escolher um número para você que esta entre 1 e 100.\n\n'
       f'Você terá {opportunity} chances de acertar o nosso número secreto ')
 
 #lista para armazenar o chutes do usuário
 attempts = []
 while opportunity > 0:
-    opportunity = play(drawn_n,pl_name,opportunity,attempts)
-
+    opportunity = play(drawn_n, pl_name, opportunity, attempts, points)
+    points -= lost_points
 
 print(f'Não foi dessa vez.... o número sorteado era {drawn_n} \n'
     'Mas vamos tentar de novo!!!')
